d31d6e367f325124712d672d407eba62e98de0c6
r-afael <roliveira1111@gmail.com>
2022-06-14 17:29:21 -0300

    feat: adiciona header para token JWT

--- src/main/java/br/com/rafael/forum/config/swagger/SpringFoxConfig.java
+++ src/main/java/br/com/rafael/forum/config/swagger/SpringFoxConfig.java
@@ -6,18 +6,32 @@ import org.springframework.context.annotation.Configuration;
 import br.com.rafael.forum.modelo.Usuario;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ParameterType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import static java.util.Collections.*;
+
 @Configuration
-public class SpringFoxConfig {                                    
-    @Bean
+public class SpringFoxConfig {
+
+	@Bean
     public Docket api() { 
+    	
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.rafael.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);                                        
-    }
+                .ignoredParameterTypes(Usuario.class)
+                .globalRequestParameters(
+                		singletonList(new springfox.documentation.builders.RequestParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para token JWT")
+                        .in(ParameterType.HEADER)
+                        .required(false)
+                        .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                        .build()));
+	}
 }
