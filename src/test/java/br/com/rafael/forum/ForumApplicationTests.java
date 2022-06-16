6efe9f1a0c1ce26fabed797ab1c70ba987dadd34
r-afael <roliveira1111@gmail.com>
2022-06-16 15:06:00 -0300

    feat: implementa profiles e testes automatizados

--- src/test/java/br/com/rafael/forum/ForumApplicationTests.java
+++ src/test/java/br/com/rafael/forum/ForumApplicationTests.java
@@ -1,6 +1,7 @@
 package br.com.rafael.forum;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 assertEquals(2,2);
 	}
 
 }
